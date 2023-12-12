058a27ddc3ef2be94d83842cbc27c2874ecb1032
Mercedes0708 <144907375+Mercedes0708@users.noreply.github.com>
2023-12-12 19:56:30 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+string = ''
+for (let x of pronoun) {
+  for (let i of adj) {
+    for (let z of noun) {
+      string = x + i + z + '.com'
+      console.log(string);
+    }
+  }
 };
